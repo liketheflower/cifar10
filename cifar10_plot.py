f0357846e4b2839368d0ae5d68c790a677675b1d
EC2 Default User <ec2-user@ip-172-31-22-81.us-west-2.compute.internal>
2017-06-28 22:25:10 +0000

    Add the VGG16 model

--- cifar10_plot.py
+++ cifar10_plot.py
@@ -1,7 +1,9 @@
+
 # Plot ad hoc CIFAR10 instances
 from keras.datasets import cifar10
 from matplotlib import pyplot
 from scipy.misc import toimage
+#matplotlib.use('GTK')
 # load data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 # create a grid of 3x3 images
