c0ef75e11edb9371b08336f8bf6dd11e7732d1bc
Jimmy Shen <jim.morris.shen@gmail.com>
2017-07-02 00:07:08 -0400

    modify the plot code to upsample the size of cifar to 224 by 224 by 3.

--- cifar10_plot.py
+++ cifar10_plot.py
@@ -1,14 +1,32 @@
-
 # Plot ad hoc CIFAR10 instances
 from keras.datasets import cifar10
 from matplotlib import pyplot
-from scipy.misc import toimage
+from scipy.misc import toimage, imresize
 #matplotlib.use('GTK')
+import numpy as np
 # load data
+
+#print(X_train.shape)
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print (X_train.shape)
 # create a grid of 3x3 images
 for i in range(0, 9):
 	pyplot.subplot(330 + 1 + i)
 	pyplot.imshow(toimage(X_train[i]))
 # show the plot
 pyplot.show()
+X_train_new = np.zeros((9,224,224,3))
+for i in range(9):
+    X_train_new[i] = imresize(X_train[i], (224,224,3), interp='bilinear', mode=None)
+#X_train_new = imresize(X_train, (X_train.shape[0],224,224,3), interp='bilinear', mode=None)
+print(X_train_new.shape) 
+#X_train_new = np.zeros((X_train[0].shape))
+for i in range(0, 9):
+        pyplot.subplot(330 + 1 + i)
+      #  X_train_new=imresize(X_train[i], (224,224,3), interp='bilinear', mode=None)
+        print(X_train_new[i].shape)
+        pyplot.imshow(toimage(X_train_new[i]))
+# show the plot
+pyplot.show()
+
+
